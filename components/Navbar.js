203d019a31ca6681a9deaf24b49c96efb76e014c
rhmnaulia <rahmanauliax@gmail.com>
2021-10-08 09:44:54 +0700

    fix: remove template literal to avoid tailwind JIT error

--- components/Navbar.js
+++ components/Navbar.js
@@ -17,15 +17,19 @@ const Navbar = () => {
   });
   return (
     <div
-      className={`${
-        navbar ? "bg-black" : "bg-transparent"
-      } sticky top-0 md:h-12 z-50 h-16 flex items-center justify-center`}
+      className={
+        navbar
+          ? "bg-black sticky top-0 md:h-12 z-50 h-16 flex items-center justify-center"
+          : "bg-transparent sticky top-0 md:h-12 z-50 h-16 flex items-center justify-center"
+      }
     >
       <div className="md:flex md:space-x-10 md:justify-center justify-center py-3">
         <div
-          className={`pt-5 md:pt-3.5 ${
-            navbar ? "w-48 md:w-48 " : "w-48 md:w-72 md:mt-12"
-          }`}
+          className={
+            navbar
+              ? "w-48 md:w-48 pt-4 md:pt-3.5 "
+              : "w-48 md:w-72 md:mt-12 pt-5 md:pt-3.5 "
+          }
         >
           <Image src={whitebrand} />
         </div>
