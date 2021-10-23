6d9840fb0fb77ff7f970fb0af639a76c5c7c0259
rhmnaulia <rahmanauliax@gmail.com>
2021-10-23 16:47:07 +0700

    feat: add title

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import Head from "next/head";
 import { motion } from "framer-motion";
 import HeroHome from "../layouts/HeroHome";
 import MusicPlayer from "../components/MusicPlayer.jsx";
@@ -68,7 +69,18 @@ const Home = () => {
   };
 
   if (isLanding) {
-    return <Landing btnAction={handleClick} />;
+    return (
+      <>
+        <Head>
+          <title>FINDELOVE</title>
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+        </Head>
+        <Landing btnAction={handleClick} />
+      </>
+    );
   }
   return (
     <motion.div
@@ -83,6 +95,11 @@ const Home = () => {
         },
       }}
     >
+      <Head>
+        <title>FINDELOVE</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      {/* SECTION */}
       <Navbar />
       <HeroHome />
       <BrideGroom />
