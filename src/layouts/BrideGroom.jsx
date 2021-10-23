ded3e170e52679a8591f03cdd911d0af61682477
rhmnaulia <rahmanauliax@gmail.com>
2021-10-23 17:26:33 +0700

    feat: add smooth scroll

--- src/layouts/BrideGroom.jsx
+++ src/layouts/BrideGroom.jsx
@@ -1,5 +1,6 @@
 import Container from "../components/Container";
 import Image from "next/image";
+import Link from "next/link";
 import groom from "../../public/static/images/groom.jpg";
 import bride from "../../public/static/images/bride.jpg";
 
@@ -55,7 +56,11 @@ export const BrideGroom = () => {
                   <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
                   <line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line>
                 </svg>
-                <p className="pl-1">groomgram</p>
+                <Link href="https://www.instagram.com/indrasuryadi_/">
+                  <a target="_blank" rel="noopener noreferrer" className="pl-1">
+                    indrasuryadi_
+                  </a>
+                </Link>
               </span>
             </div>
 
@@ -92,7 +97,11 @@ export const BrideGroom = () => {
                   <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
                   <line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line>
                 </svg>
-                <p className="pl-1">bridegram</p>
+                <Link href="https://www.instagram.com/fideliaandrean/">
+                  <a target="_blank" rel="noopener noreferrer" className="pl-1">
+                    fideliaandrean
+                  </a>
+                </Link>
               </span>
             </div>
           </div>
