226dad6e71e6040ec3f05e96ea4201ff3d084e94
andreasrobert <andreasrobb23@gmail.com>
2021-11-06 21:36:51 +0700

    feat: health protocol

--- src/layouts/HealthProtocol.jsx
+++ src/layouts/HealthProtocol.jsx
@@ -4,18 +4,18 @@ import Logo from "../../public/static/images/logo.png";
 
 export default function HealthProtocol() {
   return (
-    <div className="text-center  text-white bg-opacity-100 flex flex-col justify-center items-center healthProtocol-container">
-      <Container >
-        <div className="px-2">
+    <div className="text-center  relative text-white bg-opacity-100 flex flex-col justify-center items-center healthProtocol-container">
+        <div >
           <h1 className="text-5xl m-8 font-typography ">
             Stay Save, Stay Healthy
           </h1>
-          <p className="z-10 my-5 md:my-2 text-base md:text-xl font-serif">
-            
+          <p className="z-20 pb-14 my-5 md:my-2 text-base md:text-xl font-serif">
+            Dalam rangka Pencegahan dan pengendalian COVID-19 <br />
+           pernikahan ini akan diadakan secara daring agar tidak membuat kerumunan
             
           </p>
         </div>
-      </Container>
+      <div className="absolute h-full w-full bg-black" style={{zIndex:"-1"}}></div>
     </div>
   );
 }
