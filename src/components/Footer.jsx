e91fe93b6b3439663fcf691595515cddfe470561
Reganvalkyrie <regangirikaruna@gmail.com>
2021-12-15 23:15:00 +0700

    feat: adding footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,12 @@
 export default function Footer() {
   return (
     <footer>
-      <div className="flex flex-col items-center mt-16">
-        <div className="flex mb-8 space-x-2 text-sm text-gray-500 dark:text-gray-400">
-          <div>Virtual Wedding</div>
-          <div>{` • `}</div>
+      <div className="bg-palewhite flex flex-col items-center justify-center ">
+        <div className="flex my-3 space-x-2  text-gray-500 text-xl dark:text-gray-400">
+          <div className="font-sans">Created By</div>
+          <div className="font-typography">Karuna Wedding</div>
+
           <div>{`© ${new Date().getFullYear()}`}</div>
-          <div>{` • `}</div>
         </div>
       </div>
     </footer>
