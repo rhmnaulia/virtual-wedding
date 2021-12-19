55e6d6628d0e2412a9f36d5e9b9dd62d4b63302b
Reganvalkyrie <regangirikaruna@gmail.com>
2021-12-19 15:44:05 +0700

    fix: :art: change footer and livestream hr sty;e

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,30 @@
 export default function Footer() {
+  const Heart = () => {
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-5 w-5"
+        viewBox="0 0 20 20"
+        fill="red"
+      >
+        <path
+          fillRule="evenodd"
+          d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
+          clipRule="evenodd"
+        />
+      </svg>
+    );
+  };
+
   return (
     <footer>
-      <div className="bg-palewhite flex flex-col items-center justify-center ">
-        <div className="flex my-3 space-x-2  text-gray-500 text-xl dark:text-gray-400">
-          <div className="font-sans">Created By</div>
-          <div className="font-typography">Karuna Wedding</div>
-
+      <div className="bg-palewhite flex  items-center justify-center ">
+        <div className="flex  justify-center items-center my-3 space-x-2  text-gray-500 text-sm dark:text-gray-400">
+          <p className="font-sans">
+            We help you to create your sweet ending to a new beginning
+          </p>
+          <Heart />
+          <b className="font-typography text-xl">Karuna Wedding Planner</b>
           <div>{`© ${new Date().getFullYear()}`}</div>
         </div>
       </div>
