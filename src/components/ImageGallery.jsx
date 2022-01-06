f72c3cadf0fe7601b58e42875a15cfa13d24e543
rahmanaulia <aulia.rahman@sirclo.com>
2022-01-06 21:39:18 +0700

    fix: imageGallery array pos

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -37,7 +37,6 @@ const images = [
 ];
 
 export default function ImageGallery() {
-
   const settings = {
     dots: false,
     infinite: true,
@@ -46,59 +45,58 @@ export default function ImageGallery() {
     autoplay: true,
     autoplaySpeed: 4000,
 
-      lazyLoad: false,
-      responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
-            infinite: true,
-            dots: false,
-          },
+    lazyLoad: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: false,
         },
-        {
-          breakpoint: 600,
-          settings: {
-            infinite: true,
-            slidesToShow: 2,
-            slidesToScroll: 2,
-            initialSlide: 2,
-          },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          infinite: true,
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
         },
-        {
-          breakpoint: 480,
-          settings: {
-            infinite: true,
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            autoplaySpeed: 4000,
-          },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          infinite: true,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          autoplaySpeed: 4000,
         },
-      ],
-    };
-    return (
-      <div className="bg-palewhite pt-12 pb-8 lg:pb-0">
-        <div className="h-96 lg:h-[27rem]">
-          <h1 className="text-4xl text-secondary font-semibold text-center pb-8 font-typography">
-            Our Moments
-          </h1>
-          <Slider {...settings}>
-            {images.map((image, index) => {
-              return (
-                <div key={index} className=" mb-4  h-64 w-64  mx-auto relative">
-                  <Image
-                    src={image.src}
-                    objectFit="cover"
-                    layout="fill"
-                    className="rounded-xl"
-                  />
-                </div>
-              );
-            })}
-          </Slider>
-        </div>
+      },
+    ],
+  };
+  return (
+    <div className="bg-palewhite pt-12 pb-8 lg:pb-0">
+      <div className="h-96 lg:h-[27rem]">
+        <h1 className="text-4xl text-secondary font-semibold text-center pb-8 font-typography">
+          Our Moments
+        </h1>
+        <Slider {...settings}>
+          {images.map((image, index) => {
+            return (
+              <div key={index} className=" mb-4  h-64 w-64  mx-auto relative">
+                <Image
+                  src={image.src}
+                  objectFit="cover"
+                  layout="fill"
+                  className="rounded-xl"
+                />
+              </div>
+            );
+          })}
+        </Slider>
       </div>
-    );
-  }
+    </div>
+  );
 }
