b54a0ac513e6fc13f75381f1844be4766ef66ba7
Reganvalkyrie <regangirikaruna@gmail.com>
2022-01-06 20:01:08 +0700

    refactor: change some text to english and grammars

--- src/layouts/Livestream.jsx
+++ src/layouts/Livestream.jsx
@@ -6,22 +6,31 @@ function Livestream() {
       <h1 className=" text-secondary font-semibold  text-5xl pb-3 sm:mx-10 text-center  font-typography pt-2">
         LiveStream
       </h1>
-      <p className="text-center pt-7 pb-5  px-4 sm:text-lg sm:pt-9 sm:px-16  text-sm font-serif">
-        Dikarenakan situasi pandemi Covid-19 dan demi keamanan seluruh tamu,
-        Acara pemberkatan akan dilakukan secara virtual melalui platform zoom.
-        Silahkan bergabung dengan menekan tombol berikut. Terima Kasih.
-      </p>
-      <div className="pt-16 flex sm:flex-row flex-col justify-center">
+      <div className="livestream-message text-center pt-7 pb-5  px-4 sm:text-lg sm:pt-9 sm:px-16  text-sm font-serif">
+        <p className="pb-6">
+          With concern for the COVID- 19 Pandemic situation, our venue should
+          limit the number of celebrants. With an abundance of caution and care
+          for our guests, we have decided to conduct an intimate reception with
+          family members and closest friends.
+        </p>
+        <p className="pb-6">
+          As much as we want everyone to be able to celebrate in person, we do
+          ask if you guys can join us virtually. We sincerely appreciate your
+          love and support and hope that you celebrate in your heart with best
+          wishes from afar.{" "}
+        </p>
+        <p>With all our loves, Indra & Fidelia.</p>
+      </div>
+      <div className="pt-12 flex sm:flex-row flex-col justify-center">
         <div className="flex flex-col justify-center w-full sm:w-1/2 items-center  description-container">
           <div className="holy-matrimony-detail text-center font-serif text-base sm:text-2xl">
             <h3>Virtual Holy Matrimony</h3>
             <h2 className="pb-5">Indra & Fidelia</h2>
-            <p>Sabtu , 26 Februari 2022</p>
+            <p>Saturday, February 26th 2022</p>
             <p className="pb-5">11.00 WIB - Finish</p>
 
-            <p>Gereja ST. Petrus & Paulus</p>
-            <p>Jl.Raya Mangga Besar No. 55</p>
-            <p className="pb-6">Jakarta Barat</p>
+            <p>St. Petrus & Paulus Church</p>
+            <p className="pb-6">Jl. Raya Mangga Besar No. 55 - Jakarta Barat</p>
           </div>
         </div>
         <div className="flex flex-col items-center pb-12 lg:pb-0 w-full sm:w-1/2 video-container mb-5">
--- src/layouts/OurStory.jsx
+++ src/layouts/OurStory.jsx
@@ -18,30 +18,27 @@ function OurStory() {
         />
       </div>
       <div className="story flex flex-col w-10/12 lg:w-3/4 text-center text-sm md:text-lg md:px-40 font-serif">
-        <p>
-          We knew each other first as a colleague in the same Catholic youth
-          community.
-        </p>
+        <p>We knew each other first as a colleague</p>
+        <p>in the same Catholic youth community.</p>
         <p className="mb-1 pb-3 ">
           {" "}
-          We never expect that we could found each other in the class where we
+          We never expect that we could find each other in a class where we
           barely talked to each other, after class or before, let alone we
           choose to spend the rest of our life together.{" "}
         </p>
         <p className="mb-1 pb-3 ">
-          We have a lot of differences in personality, way of thinking, and
-          sometimes in the way we see things in life but we believe those
-          enriched our relationship.
+          We have a lot of differences in personality, and sometimes in ways of
+          thinking, but we believe those enriched our relationship.
         </p>
-        <p>
+        <p className="mb-1 pb-3">
           {" "}
           Indra is the type of person who is reserved, calm, and sometimes
           complicated while Fidel is the type of person who is seemly easy to
-          engage with, fun-loving, lively and energetic.
+          engage with, fun-loving, and lively.
         </p>
         <p>
-          Although there are many differences, we both managed to bare
-          ourselves, developing and commiting to become a better partner for
+          Although there are many differences, we both managed to bear
+          ourselves, developing and committing to becoming a better partner for
           each other, for the rest of our life.
         </p>
       </div>
